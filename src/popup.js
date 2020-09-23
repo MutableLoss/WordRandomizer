80bb6bdb3217aaac40b41482f755efc34d5f5418
Dennis Brown <vilesyn@gmail.com>
2020-09-22 19:48:39 -0500

    add popup html

--- src/popup.js
+++ src/popup.js
@@ -1,12 +1,17 @@
 (function() {
   const WordList = new wordRandomizer.WordList;
+
+  const bkg = chrome.extension.getBackgroundPage();
   const port = chrome.runtime.connect({ name: "randomizer" });
 
   function createBlock() {
     let wordList = document.querySelector('.word-list');
-    let wordBlock = document.createElement('a');
+    let wordBlock = document.createElement('span');
     wordBlock.setAttribute('target', '_inbox');
     wordBlock.setAttribute('class', 'button message-block');
+    wordBlock.textHtml = window.setWord
+
+    bkg.console.log(`pop: ${WordList.getWord()}`)
 
     wordList.append(wordBlock);
   }
@@ -14,7 +19,9 @@
   function populateList() {
     WordList.initList(words => createBlock());
 
-    chrome.runtime.sendMessage({}, function(response) {});
+    chrome.runtime.sendMessage({}, function(response) {
+      console.log(`popup: ${response}`)
+    });
   }
 
   populateList();
