2d9dfb94cc199d9302b096f21d35d19840a723ae
Dennis Brown <vilesyn@gmail.com>
2020-09-24 19:58:36 -0500

    add the abilty to cycle through list of words

--- src/popup.js
+++ src/popup.js
@@ -4,33 +4,63 @@
   const port = chrome.runtime.connect({ name: "randomizer" });
 
   var settings, word, wordSet;
+  var currentWord = 0;
+
+  function nextWord() {
+    currentWord++;
+
+    wipeBlock();
+    createBlock();
+  }
+
+  function wipeBlock() {
+    let wordList = document.querySelector('.word-list');
+    wordList.setAttribute('height', '250px');
+
+    let wordDiv = document.querySelector('.word');
+    let example = document.querySelector('.word-example');
+    let button = document.querySelector('.next-button');
+
+    if (example) {
+      wordDiv.removeChild(example);
+    }
+    wordDiv.removeChild(button);
+  }
 
   function createBlock() {
     let wordDiv = document.querySelector('.word');
-    let wordBlock = document.createElement('span');
-    wordBlock.setAttribute('class', 'word-block word-question');
-    wordBlock.innerHTML = word.word;
+    let wordBlock = document.querySelector('.word-question');
+    wordBlock.innerHTML = wordSet[currentWord].word;
 
-    let answerBlock = document.createElement('span');
-    answerBlock.setAttribute('class', 'word-block word-answer');
-    answerBlock.innerHTML = word.meaning;
+    let answerBlock = document.querySelector('.word-answer');
+    answerBlock.innerHTML = wordSet[currentWord].meaning;
 
-    wordDiv.append(wordBlock);
-
-    if (word.example && word.example.trim()) {
+    if (wordSet[currentWord].example && wordSet[currentWord].example.trim()) {
       let exampleBlock = document.createElement('span');
       exampleBlock.setAttribute('class', 'word-block word-example');
-      exampleBlock.innerHTML = word.example;
+      exampleBlock.innerHTML = wordSet[currentWord].example;
 
-      wordDiv.append(exampleBlock);
+      wordDiv.insertBefore(exampleBlock, answerBlock);
     }
 
-    wordDiv.append(answerBlock);
+    if (currentWord < wordSet.length) {
+      let nextButton = document.createElement('button')
+      nextButton.setAttribute('class', 'next-button');
+      nextButton.id = 'next';
+      nextButton.innerText = 'Next';
+
+      wordDiv.insertBefore(nextButton, wordBlock);
+    }
   }
+
+  document.addEventListener('click', function(e) {
+    if (e.target && e.target.id === 'next') {
+      nextWord();
+    }
+  })
 
   chrome.runtime.sendMessage({ name: 'clear-badge' }, function(response) {});
   chrome.runtime.sendMessage({ name: 'get-word' }, function(response) {});
-
 
   chrome.runtime.onMessage.addListener(function(message) {
     if ('action' in message && message.action == 'update-word') {
