5629f2955d685269817372c804b9bf9cf6c9caf3
Dennis Brown <vilesyn@gmail.com>
2020-09-26 13:33:35 -0500

    update prefences on startup and add local getter

--- src/preferences.js
+++ src/preferences.js
@@ -13,12 +13,21 @@
   };
 
   wordRandomizer.Preferences.prototype = {
+    constructor: function() {
+      Object.keys(this.settings.options).map(key => {
+        this.settings[key] = this.get(key);
+      });
+    },
+    getLocal: function(prefKey) {
+      this.settings.options[prefKey];
+    },
     get: function(prefKey, cb) {
       chrome.storage.sync.get(this.settings, store => {
         cb(store.options[prefKey]);
       });
     },
     set: function(prefKey, value) {
+      this.settings.options[prefKey] = value;
       chrome.storage.sync.get(this.settings, store => {
         store.options[prefKey] = value;
         chrome.storage.sync.set(store);
